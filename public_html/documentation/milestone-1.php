4882037fda377dff95c7fd3c705a3a282c9c05bf
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-20 00:43:23 -0600

    finished use case and added goal purpose and audience sections

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
+
 <html>
+
 	<head>
+
 		<meta charset="utf-8">
 
 		<!--This is the title-->
+
 		<title>James Montoya</title>
 
 	</head>
@@ -12,17 +16,35 @@
 
 		<!--This is the header-->
 		<header>
+
 			<h1>Art Fight: New Mexico</h1>
+
 		</header>
 
 		<!--This is the main page content-->
+
 		<main>
+
+			<!--this is the purpose of the site-->
 
 			<h2>Purpose</h2>
 
 				<p>The purpose of this website is to inform the public about the Art Fight event held once a month at Tractor Brewing Co. Wells Park.  It is a live art competition where the beer drinking public decide the winner.  Each artist creates a work of art from scratch during the three hour event based on a theme.  During that time every drink purchased comes with a free ticket that patrons use to vote for their favorite artist of the night. At the end of the event we count the votes and declare a winner.  The action isn't over though until we auction off the artwork that was made that night. The proceeds are split between the artist and some sort of cause. Past Art Fights have helped the Muertos y Marigolds Parade, PACA, Babe's and Bullies, the Bees and Seeds Festival, Southwest Organizing Project and many more. Art Fight has been going strong for two years and continues to grow. The website will aim to sign people up for a mailing list to keep them informed of events and pertinent information.</p>
 
-			<!--begin sention 1-->
+			<!--this is the audience-->
+
+			<h3>Audience</h3>
+
+				<p>Art Fight's audience is as varied as the population of Albuquerque.  They range in age from 21-65. They enjoy craft beer and local art. Some come just for the art and some come just for the beer but most come for both.  Most are tech savy and use apps often.
+				</p>
+
+			<!--this is the goal-->
+			<h4>Goal</h4>
+
+				<p>One major goal is to inform the user of the rules of Art Fight, upcoming dates and themes, show a leader board of past winners and link to their sites, share links of the organizations we help, and sign people up for an e-mail newsletter.</p>
+
+			<!--begin persona section-->
+
 			<section>
 
 				<p><b>Name</b>: Bobby Whright</p>
@@ -37,18 +59,17 @@
 
 				<p><b>Needs and Frustrations</b>: He is looking to buy original artwork to add to his collection.  He enjoys the galleries in Santa Fe and old town Albuquerque but feels the artwork is somewhat boring and the styles out of touch with today.  He wants to find a gallery, or venue, or event, or something that makes him excited about art again. He heard of the event at Tractor Brewing called Art Fight from the secretary at his work.  She thought it would intrest him because of his love of art.  Although he was skeptical of the quality of art that can be made in a brewery, his secretary showed him a picture of a piece done by local artist Cloudface and he was pleasantly suprised that it was actually good.  After he heard about the price she paid he was even more excited to see the event for himself so he has come to the site looking for more info about the next Art Fight.</p>
 
+				<!--begin use case section-->
+
 				<h3>Use Case</h3>
 
-				<p>After hearing about the Art Fight from his secretary Bobby </p>
+				<p>After a meeting with a client and waiting for his next appointment Bobby stares at his office wall across from his desk.  The painting that hangs on the wall is one from his own collection.  It is a large canvas painted by renowned painter of Santa Fe art history, William Schuster.  It has hung on that wall for years and every now and then another, often very much older, client comments on how they enjoy the painting.  Looking at it now he feels it's looking more and more like those clients, old.  </p>
 
-				<p></p>
+				<p>Hearing about the Art Fight from his secretary, Bobby decides to try to find out more about this art competition she was talking about.  He turns on his lap top and googles Art Fight.  Clicking on the first link brings him to the site.</p>
 
-				<p></p>
+				<p>Once there he sees a list of past events along with a slideshow of pictures showing the event in full swing.  He clicks through the slideshow noticing the artist Cloudface that his secretary showed him.  Along with Cloudface he is impressed with several of the artists who painted at past Art Fight's. The quality of work far surpassing his expectations. As he scrolls down to the middle of the page he watches a promo video showing the concept of the night and the artists who participate.</p>
 
-
-			</section>
-
-			<section>
+				<p>Seeing all the art and apparent fun had by the crowd, Bobby is excited to go to the next show and participate in the auction.  As he scrolls farther down the page he sees a form to sign up for the mailling list to stay informed of current Art Fight news.  He fills out the form and instantly gets a confirmation email notice indicating he has finished the sign up process and will no longer miss Art Fight news.</p>
 
 			</section>
 

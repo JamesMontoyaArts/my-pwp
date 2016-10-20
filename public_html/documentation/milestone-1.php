e6e03ec68d69e06af62d67895b05f59a16f9f5e2
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-19 22:54:52 -0600

    added persona and started use case html

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<!--This is the title-->
+		<title>James Montoya</title>
+
+	</head>
+
+	<body>
+
+		<!--This is the header-->
+		<header>
+			<h1>Art Fight: New Mexico</h1>
+		</header>
+
+		<!--This is the main page content-->
+		<main>
+
+			<h2>Purpose</h2>
+
+				<p>The purpose of this website is to inform the public about the Art Fight event held once a month at Tractor Brewing Co. Wells Park.  It is a live art competition where the beer drinking public decide the winner.  Each artist creates a work of art from scratch during the three hour event based on a theme.  During that time every drink purchased comes with a free ticket that patrons use to vote for their favorite artist of the night. At the end of the event we count the votes and declare a winner.  The action isn't over though until we auction off the artwork that was made that night. The proceeds are split between the artist and some sort of cause. Past Art Fights have helped the Muertos y Marigolds Parade, PACA, Babe's and Bullies, the Bees and Seeds Festival, Southwest Organizing Project and many more. Art Fight has been going strong for two years and continues to grow. The website will aim to sign people up for a mailing list to keep them informed of events and pertinent information.</p>
+
+			<!--begin sention 1-->
+			<section>
+
+				<p><b>Name</b>: Bobby Whright</p>
+
+				<p><b>Age</b>: 42 </p>
+
+				<p><b>Profession</b> Full time attorney for local established lawfirm.  Avid art collector with several pieces from well established artists.  He has decided to purchase more local art to add to his collection.</p>
+
+				<p><b>Technology</b>: Macbook pro 15" laptop with retina display, iPhone 7 and a 42" hi-def Samsung television at home. Broadband internet through City Link </p>
+
+				<p><b>Attitudes and Behaviors</b>: He is an avid art collector who tries to expand his collection of original artwork any chance he gets.  He supports local art initiatives whenever asked and also sponsors a college scholarship for chicano artists seeking higher education in the visual arts.  He is involved with many local organizers and has done pro bono work for many of the non-profits that are close to his heart.  He believes in shopping local whenever he can.</p>
+
+				<p><b>Needs and Frustrations</b>: He is looking to buy original artwork to add to his collection.  He enjoys the galleries in Santa Fe and old town Albuquerque but feels the artwork is somewhat boring and the styles out of touch with today.  He wants to find a gallery, or venue, or event, or something that makes him excited about art again. He heard of the event at Tractor Brewing called Art Fight from the secretary at his work.  She thought it would intrest him because of his love of art.  Although he was skeptical of the quality of art that can be made in a brewery, his secretary showed him a picture of a piece done by local artist Cloudface and he was pleasantly suprised that it was actually good.  After he heard about the price she paid he was even more excited to see the event for himself so he has come to the site looking for more info about the next Art Fight.</p>
+
+				<h3>Use Case</h3>
+
+				<p>After hearing about the Art Fight from his secretary Bobby </p>
+
+				<p></p>
+
+				<p></p>
+
+
+			</section>
+
+			<section>
+
+			</section>
+
+		</main>
+
+	</body>
+
+</html>

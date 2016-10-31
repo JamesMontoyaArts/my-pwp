597ae1a08e855ad22451067084cf4f789601e14f
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-31 08:23:44 -0600

    added images of wireframes to the milestone-2.php file

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -16,12 +16,11 @@
 
 		<h2>Content Strategy</h2>
 
-		<p>The content of the site will focus on the monthly event called Art Fight. It will be divided into 6 sections.  The first section will explaing the rules of Art Fight and give a little background into it's history.  The next section will feature a gallery of images and fliers from past events.  Followed by a section featuring a promotional video of Art Fight.   </p>
+		<p>The content of the site will focus on the monthly event called Art Fight. It will be divided into 6 sections.  The first section will explaing the rules of Art Fight and give a little background into it's history.  The next section will feature a gallery of images and fliers from past events, followed by a section featuring a promotional video of Art Fight hosted on a site such as youtube or vimeo.  The next section will explain our auction process and also give information and links to our partners we help in the community.  The last section will be a contact form and a footer will have links to the various social networks Art Fight is a part of.</p>
 
+		<img src="img/page-mock-up.jpg" alt="wireframe full size" height="100%" width="100%">
 
-		<ul>
+		<img src="img/website-mock-up-mobile.jpg" alt="mobile wireframe" height="100%" width="100%">
 
-			<li></li>
-		</ul>
 	</body>
 </html>

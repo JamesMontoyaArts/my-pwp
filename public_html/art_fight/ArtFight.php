874fb3aeee1d2dbaba89096e453a887d4f7c76ef
James Montoya <jamesmontoyaarts@gmail.com>
2016-11-10 21:27:44 -0700

    added bootstrap and created sections and div tag in section 1

---
+++ public_html/art_fight/ArtFight.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content=""IE="edge">
+		<meta name="viewport" content=""width="device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Art Fight</title>
+
+	</head>
+
+	<body>
+
+		<!--Start of Section 1-->
+
+		<section id="section1">
+
+			<h1>Rules of Art Fight</h1>
+
+			<div class="rules">Rules of Art Fight</div>
+			<div class=""
+		</section>
+
+		<!--Start of Section 2-->
+
+		<section id="section2">
+
+		</section>
+
+		<!--Start of Section 3-->
+
+		<section id="section3">
+
+		</section>
+
+		<!--Start of Section 4-->
+
+		<section id="section4">
+
+		</section>
+
+		<!--Start of Section 5-->
+
+		<section id="section5">
+
+		</section>
+
+		<!--Start of Section 6-->
+
+		<section id="section6">
+
+		</section>
+
+	</body>
+</html>

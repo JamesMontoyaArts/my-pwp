782d733d597b41cd33010d14bc6b6e1f0e570daf
James Montoya <jamesmontoyaarts@gmail.com>
2016-11-11 20:45:31 -0700

    added jquery compile cdn link at bottom of the page

--- public_html/ArtFight.php
+++ public_html/ArtFight.php
@@ -4,8 +4,8 @@
 
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="x-ua-compatible" content=""IE="edge">
-		<meta name="viewport" content=""width="device-width, initial-scale=1">
+		<meta http-equiv="x-ua-compatible" content="" IE="edge">
+		<meta name="viewport" content="" width="device-width, initial-scale=1">
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -15,6 +15,13 @@
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!--Pixi.js graphics accelerator-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.0.0/pixi.min.js"></script>
+        
+        <!-- Load my style sheet -->
+        
+		<link rel="stylesheet" type="css/text" href="css/custom.css">
 
 		<title>Art Fight</title>
 
@@ -28,8 +35,19 @@
 
 			<h1>Rules of Art Fight</h1>
 
-			<div class="rules">Rules of Art Fight</div>
-			<div class=""
+			<div class="container-fluid">
+
+				<div class="row">
+
+				<div class="rules">Rules of Art Fight</div>
+				<div class="Auction">Live Auction</div>
+				<div class="Gallery">Past Fights</div>
+				<div class="Partners">Community Partners</div>
+
+				</div>
+
+			</div>
+
 		</section>
 
 		<!--Start of Section 2-->
@@ -61,6 +79,12 @@
 		<section id="section6">
 
 		</section>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
 
 	</body>
 </html>

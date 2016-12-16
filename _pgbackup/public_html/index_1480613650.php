645e5bd3d3ac64852a7bf6583684bd988202325b
James Montoya <jamesmontoyaarts@gmail.com>
2016-12-16 10:17:17 -0700

    fixing contact form again

---
+++ _pgbackup/public_html/index_1480613650.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="" IE="edge">
+		<meta name="viewport" content="" width="device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Load my style sheet -->
+		<link rel="stylesheet" type="text/css" href="css/custom.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!--Latest version of jquery-->
+		<script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+		<container id="top">
+			<div class="header-row">
+			<header>
+				<div class="span12">
+				<img src="img/header.png" class=".img-responsive header-img">
+				</div>
+			</header>
+			</div>
+		</container>
+
+	<section id="section0">
+		<container id="yoArtFight">
+			<div class="container">
+				<div class="box">
+
+				<img src="img/yoArtFight.png" class="img-responsive">
+
+				</div>
+			</div>
+		</container>
+	</section>
+
+		<section id="section1">
+			<container id="rules">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+						</div>
+					</div>
+				</div>
+			</container>
+		</section>
+
+		<container>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="//www.youtube.com/embed/J6p77Hy4uv0" frameborder="0" allowfullscreen"></iframe>
+					</div>
+				</div>
+			</div class="col-md-6">
+		</container>
+
+	</body>
+</html>
